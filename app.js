3bfc08d2f3066d283674ed944435a4fdb17ea09b
Skynii <82392687+Skynii@users.noreply.github.com>
2021-07-09 15:23:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const books=require("./database")
 
-//Objetivo: pegar o input de categorias
+//Objetivo: pegar o input de categorias e criar fitros
+
+const books=require("./database")
 
 const readline=require('readline-sync')
 
-const enrtry= readline.question('Deseja buscar um livro? S/N ')
+const entry= readline.question('Deseja buscar um livro? S/N ')
 
-if(enrtry.toLocaleUpperCase() === 'S'){
-    console.log('Essas são as categorias disponiveis:')
-    console.log('Romance', '- Terror', '- Policial')
+if(entry.toUpperCase() === 'S'){
+    console.log('Essas são as categorias disponiveis: ')
+    console.log('Romance', '- Terror', '- Policial', '- Marketing e Vendas ', '- Desenvolvimento pessoal ', '- Filosofia')
 
-    const entradaCategoria=readline.question('Qual categoria você escolhe?')
-    const retorno = books.filter(book => book.category === entradaCategoria)
+    const categoryEntry= readline.question('Qual categoria você escolhe?  ')
+    const retorno = books.filter(book => book.category === categoryEntry).sort((a,b) => a.name - b.name) 
 
     console.table(retorno)
 }else{
-    const booksOrder= books.sort((a,b)=>a.pages -b.pages)
+    const booksOrder= books.sort((a,b)=>a.author - b.author)
     console.log('Esse são todos os livros disponiveis: ')
     console.table(booksOrder)
 }
