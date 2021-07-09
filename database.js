679e1319b32909abd5339bd22d80ce9f316604cf
Skynii <82392687+Skynii@users.noreply.github.com>
2021-07-09 15:24:17 -0300

    Update database.js

--- database.js
+++ database.js
@@ -46,6 +46,33 @@ const books = [
     price: "R$ 34,90",
     category: "Terror",
   },
+  {
+    id:6,
+    name: "A Sultil Arte de Ligar o Foda-se",
+    author: "Mark Manson",
+    pages: 224,
+    year: 2017,
+    price: "R$ 39,90",
+    category: "Desenvolvimento Pessoal",
+  },
+  {
+    id:7,
+    name: "Seja Foda!",
+    author: "Caio Carneiro",
+    pages: 192,
+    year: 2017,
+    price: "R$ 44,90",
+    category: "Marketing e Vendas",
+  },
+  {
+    id:8,
+    name: "Quem sabe faz a Hora!",
+    author: "Mario Sergio Cortella",
+    pages: 172,
+    year: 2021,
+    price: "R$ 34,90",
+    category: "Filosofia",
+  },
 ];
 
-module.export =books //exportação do array para App.js
+module.exports = books //exportação do array para App.js
